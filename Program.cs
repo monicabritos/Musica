fbb4210d4d46a1c8b0daeaa48d81310d37d18a67
Mônica <monicambs@1@gmail.com>
2022-09-11 15:56:00 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.BackgroundColor = ConsoleColor . Green;
+Console.ForegroundColor = ConsoleColor . Yellow;
+Console.WriteLine("Feliz demais - Aline Barros");
+Console.WriteLine();
+Console.WriteLine("Eu estou a caminhar");
+Console.WriteLine("E vejo tudo diferente");
+Console.WriteLine("Eu nasci de novo");
+Console.WriteLine("E agora vivo plenamente");
+Console.WriteLine();
+Console.WriteLine("O meu passado deixei pra trás");
+Console.WriteLine("E a culpa nãome encontra mais");
+
+Console.WriteLine("Eu sei que Deus tudo novo faz");
+Console.WriteLine("Tudo novo faz");
+Console.WriteLine();
+Console.ForegroundColor =  ConsoleColor . Red;
+ Console.WriteLine("Feliz demais");
+ Console.WriteLine("Seu amor te encontrou");
+Console.WriteLine("Sua paz me alcançou");
+Console.WriteLine("Ohhhhh, Ohhhhh, Ohhhhh");
+
